684d18459f48327291db9342b81073a4571dbb5a
abel.cejas <abel.cejas@crowdaronline.com>
2025-05-25 18:25:23 -0300

    fix bug deploy 3

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const serverless = require('serverless-http');
+const path = require('path');
 
 app.use(express.json());
 app.use(require('cors')({ optionsSuccessStatus: 200 }));
 app.use(express.static(path.join(__dirname, '../public')));
 
-const path = require('path');
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
